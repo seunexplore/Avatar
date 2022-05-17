33689318ed6caf0e0f2937834824786dddc4a7c8
seunexplore <bamgbadeoluwaseunc@gmail.com>
2022-05-17 13:05:49 +0100

    initial commit

---
+++ Seun.py
@@ -0,0 +1,155 @@
+
+# my_var = "5"
+# print(type(my_var))
+# my_int = float(my_var)
+# print(type(my_int))
+
+# a=5
+# b=7
+# c=-1
+
+# numerator = -b + (b**2 - 4*a*c)**0.5
+# denom = 2*a
+# print(numerator/denom)
+
+# from tkinter.messagebox import QUESTION
+
+
+# pi = 3.142
+# r = 5
+# volume = (4/3)*pi*(r**3)
+# print(volume)
+
+# book_price = 24.95
+# discount = 0.6
+# Ship_1 = 3
+# Ship_2 = 0.75
+# no_of_books = 60
+
+# total_book_price =book_price*discount*no_of_books 
+# total_checkout_price = total_book_price+Ship_1+(Ship_2*59)
+
+# print(total_checkout_price)
+
+
+# QUESTION 3
+
+
+
+# start_time = 6*3600 + (52*60)
+# easy_time = ((8*60) +15)*2
+# tempo_time = ((7*60)  +12)*3
+
+# run_time = easy_time + tempo_time
+
+
+# end_time = start_time + run_time
+# end_time_hr = end_time//3600
+# end_time_min =(end_time%3600)//60
+# end_time_sec = (end_time%3600)%60
+
+# print(f"{end_time_hr}:{end_time_min}:{end_time_sec}am")
+
+# string1 = 'This is a string'
+# string2 = 'I love this string'
+# #  string slicing
+# print(string1[3:8:1])
+
+# print(string1, string2)
+# print(string1+" "+string2)
+# STRING SUBSCRIPTING[]
+# print(string1[-1])
+
+# interger1 = 987654321
+# interger2 = 123456789
+
+# print(interger1[3])
+
+# STRING FORMATTING
+# name = "ZOE"
+# print(f"My name is {name}")
+
+# name = input("Enter your name: ")
+
+# print(f"""Welcome , {name}.
+
+# You have signed up successfully.
+
+# Cheers,
+# Newlife Team.""")
+
+# first_name = input("First name: ")
+# last_name = input("Last name: ")
+
+# username = last_name[:3]+first_name[-2:]
+
+# print(f"""Welcome , {first_name}.
+# Your account has been successfully created and your username is {username}.
+
+# kindly proceed to your portal to login.
+
+# Cheers,
+# Newlife.""")
+# STRING METHODS
+
+# name = input("Enter your name:\n::>>") 
+
+# text = """ An alternative is designed development, in which high-level insight 
+# into the problem can make the programming much eaiser. In this case, the insight is a 
+# that Time object is really a three-digit number in base 60"""
+
+# search_for = input(">").lower()
+# print(f"{text.lower().count(search_for)} result(S) found")
+# print(text.lower().replace(search_for, search_for.upper()))
+ 
+#  Solution To Assignments
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution 1
+
+a = 5
+b = 10
+c = 8
+
+avg = (a+b+c)/3
+
+print(f"The average of {a},{b},{c} is: {avg}")
+
+# Solution 2
+
+sentence = "seun is a newbie in tech and she find it interesting"
+print(sentence.capitalize())
+print(sentence)
+
+# Solution 3
+
+sentence = "I am learning python"
+change_sentence = sentence.replace("I am", "You are")
+print(change_sentence)
+
+# Solution 4
+
+string = "I hope you had fun today in class"
+print('Number of occurence of a : ', string.count('a'))
+
+
+
+
+
+
+ 
